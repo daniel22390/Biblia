3b147ba6f541bade1c62c6b1b7a235def28e3549
Daniel Gomes de Oliveira <daniel22390@hotmail.com>
2018-01-11 00:55:43 -0200

    Criada a paginação pelo javascript

--- biblia/app/controllers/principal_controller.rb
+++ biblia/app/controllers/principal_controller.rb
@@ -1,13 +1,13 @@
 class PrincipalController < ApplicationController
 	before_action :require_authentication, :only => [:envia_termos]
+	attr_accessor :array_resposta
 
 	def envia_termos
 		
 		@resposta_principal = Principal.new(params)
 
 		if @resposta_principal.valid?
-			@array_resposta = @resposta_principal.busca_exata
-			@resultado = @array_resposta[0...10]
+			@resultado = @resposta_principal.busca_exata
 
 		else
 			@resultado = Hash.new
