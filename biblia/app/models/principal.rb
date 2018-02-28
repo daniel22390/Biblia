f5cc6716423a44885c6aada3eb7fb2fcc1bb09a5
Daniel Gomes de Oliveira <daniel22390@hotmail.com>
2018-02-27 23:47:43 -0300

    removido comentarios, adcionado squish no select e exibindo log

--- biblia/app/models/principal.rb
+++ biblia/app/models/principal.rb
@@ -23,81 +23,10 @@ class Principal
 		@resultado_secundario = Hash.new
 	end
 
-	def busca
-		# busca_versiculo
-		# if @exata
-		# 	busca_exata
-		# end
-		# gera_resultado
-		arq = File.new("arquivo.txt", "w")
-
-		 Versiculo_has_termo.all.each do |hash|
-		 	termo = Termo.find(hash.as_json["termo_id"]).as_json["termo"]
-			versiculo = Versiculo.find(hash.as_json["versiculo_id"]).as_json["texto"]
-			versiculo = versiculo.mb_chars.downcase!
-			aparicao = 0
-
-			@caracteres.each do |caracter|
-				versiculo = versiculo.tr(caracter, "")
-			end
-
-			array_termos = versiculo.split
-
-			array_termos.each do |palavra|
-				if(I18n.transliterate(palavra) == I18n.transliterate(termo))
-					aparicao += 1
-				end
-			end
-
-			if(aparicao == 0)
-				if(I18n.transliterate(termo).include? "-")
-					aparicao += 1
-				end
-			end
-
-			if aparicao == 0
-
-				versiculo = Versiculo.find(hash.as_json["versiculo_id"]).as_json["texto"]
-
-				versiculo = versiculo.tr("-", " ")
-
-				array_termos = versiculo.split
-
-				array_termos.each do |palavra|
-					if(I18n.transliterate(palavra) == I18n.transliterate(termo))
-						aparicao += 1
-					end
-				end
-			end
-
-			#if aparicao == 0
-			#	puts termo
-			#	puts versiculo
-			#	puts "-------"
-			#end
-
-			versiculo_id = hash.as_json["versiculo_id"]
-			termo_id = hash.as_json['termo_id']
-			aparicoes = aparicao
-
-			arq.puts "UPDATE versiculo_has_termos SET aparicoes = #{aparicoes} WHERE versiculo_id = #{versiculo_id} AND termo_id = #{termo_id};";
-
-			#conn = ActiveRecord::Base.connection
-			#result = conn.execute "UPDATE versiculo_has_termos SET aparicoes = #{aparicao} WHERE versiculo_id = #{hash.as_json['versiculo_id']} AND termo_id = #{hash.as_json['termo_id']}" 
-				
-		 end
-
-		 arq.close unless file.closed?
-
-	end
-
 	def busca_exata
 		termos = @termos.split
 
-		@totalAparicao = {}
-		@totalAparicaoSinonimos = {}
 		@texto_versiculos = Array.new
-		@texto_versiculos_sinonimos = {}
 
 		busca_versiculo
 
@@ -107,22 +36,12 @@ class Principal
 				termo = Termo.where(:termo => value).first
 
 				if @sinonimo && termo
-
 					sinonimos = Termo_has_sinonimo.where(:sinonimo => termo).as_json
 					sinonimos.each do |sinonimo|
 						valor_sinonimo = Termo.find(sinonimo["termo_id"]).as_json
 						versiculos = Versiculo_has_termo.where(:termo => sinonimo["termo_id"]).as_json
-						if versiculos.length > 0
-							@totalAparicao[valor_sinonimo["termo"]] = (versiculos.length * 5).to_f
-						else
-							@totalAparicao[value] = 0.0
-						end
 						versiculos.each do |verso|
 							if !@hashCompletas[verso["versiculo_id"]]
-
-								# result = Versiculo.find(verso["versiculo_id"]).as_json
-								# texto = result["texto"]
-								# @texto_versiculos[result["idVersiculo"]] = result
 
 						  		if !@numeroExatas[verso["versiculo_id"]]
 									@numeroExatas[verso["versiculo_id"]] = {};
@@ -157,17 +76,10 @@ class Principal
 					antonimos.each do |antonimo|	
 						valor_antonimo = Termo.find(antonimo["termo_id"]).as_json
 						versiculos = Versiculo_has_termo.where(:termo => antonimo["termo_id"]).as_json
-						if versiculos.length > 0
-							@totalAparicao[valor_antonimo["termo"]] = (versiculos.length * 5).to_f
-						else
-							@totalAparicao[value] = 0.0
-						end
+
 						versiculos.each do |verso|
 							if !@hashCompletas[verso["versiculo_id"]]
 
-								# result = Versiculo.find(verso["versiculo_id"]).as_json
-								# texto = result["texto"]
-								# @texto_versiculos[result["idVersiculo"]] = result
 						  		if !@numeroExatas[verso["versiculo_id"]]
 									@numeroExatas[verso["versiculo_id"]] = {};
 								end
@@ -208,16 +120,12 @@ class Principal
 							verbo = Termo.find(verbo_hash["termo_id"])
 
 							if(verbo['termo'] != termo['termo'])
-								
 								versiculos =  Versiculo_has_termo.where(:termo => verbo["idTermo"]).as_json
 
 								if ((verbo["termo"] != flexao["termo"]) || controle_flexao)
 									versiculos.each do |verso|
 										if !@hashCompletas[verso["versiculo_id"]]
 
-											# result = Versiculo.find(verso["versiculo_id"]).as_json
-											# texto = result["texto"]
-											# @texto_versiculos[result["idVersiculo"]] = result
 									  		if !@numeroExatas[verso["versiculo_id"]]
 												@numeroExatas[verso["versiculo_id"]] = {};
 											end
@@ -254,22 +162,17 @@ class Principal
 
 				if @radical && termo
 					retorno = gera_radical(Termo.where(:termo => value).first.as_json)
-					radical = Radical.where(:radical => retorno).first.as_json
-
+					radical = Radical.where(:radical => retorno.squish).first.as_json
 					termos_radicais = Termo.where(:radical => radical["idRadical"]).as_json
 
 					termos_radicais.each do |termo_radical|
 
 						if termo_radical['termo'] != termo["termo"]
-
 							versiculos =  Versiculo_has_termo.where(:termo => termo_radical["idTermo"])
 
 							versiculos.each do |verso|
 								if !@hashCompletas[verso["versiculo_id"]]
 
-									# result = Versiculo.find(verso["versiculo_id"]).as_json
-									# texto = result["texto"]
-									# @texto_versiculos[result["idVersiculo"]] = result
 							  		if !@numeroExatas[verso["versiculo_id"]]
 										@numeroExatas[verso["versiculo_id"]] = {};
 									end
@@ -301,11 +204,7 @@ class Principal
 
 				if termo && @exata
 					versiculos = Versiculo_has_termo.where(:termo => termo).as_json
-					if versiculos.length > 0
-						@totalAparicao[value] = (versiculos.length * 10).to_f
-					else
-						@totalAparicao[value] = 0.0
-					end
+
 					versiculos.each do |verso|
 						if !@hashCompletas[verso["versiculo_id"]]
 							if !@numeroExatas[verso["versiculo_id"]]
@@ -316,9 +215,6 @@ class Principal
 								@numeroExatas[verso["versiculo_id"]][value] = 0.0;
 							end
 
-							# result = Versiculo.find(verso["versiculo_id"]).as_json
-							# texto = result["texto"]
-							# @texto_versiculos[result["idVersiculo"]] = result
 						  	@numeroExatas[verso["versiculo_id"]][value] += verso["aparicoes"] * (@current_user["pesoExata"].to_f * 10)
 
 							if(!@resultado_secundario[verso["versiculo_id"]])
@@ -342,43 +238,6 @@ class Principal
 		@pesoExatas = {}
 
 		@numeroExatas.each do |key, versiculo|
-			# pesos = []
-			# somatorio = 0.0
-			# versiculo.each do |key2, termo|
-			# 	if @totalAparicao[key2] > 0
-			# 		numero = (31097.0 / @totalAparicao[key2]).to_f
-			# 	else
-			# 		numero = 0.0
-			# 	end
-			# 	peso = ((1 + Math.log2(termo)) *  Math.log2(numero)).to_f
-			# 	pesos.push(peso)
-			# 	if @totalAparicao[key2] > 0
-			# 		somatorio += (peso * Math.log2(31097.0 / @totalAparicao[key2])).to_f
-			# 	end
-			# end
-
-			# multiplicatorio = 0.0
-			# multiplicatorio1 = 0.0
-			# multiplicatorio2 = 0.0
-
-			# pesos.each do |valor|
-			# 	multiplicatorio1 += (valor**2).to_f
-			# end
-			# multiplicatorio1 = (Math.sqrt(multiplicatorio1)).to_f
-
-			# @totalAparicao.each do |key, valor|
-			# 	if(valor == 0)
-			# 		multiplicatorio2 += 0.0
-			# 	else
-			# 		multiplicatorio2 += (Math.log2(31097.0 / valor)**2).to_f
-			# 	end
-			# end
-
-			# multiplicatorio2 = (Math.sqrt(multiplicatorio2)).to_f
-
-			# multiplicatorio = (multiplicatorio1 * multiplicatorio2).to_f
-
-			# @pesoExatas[key] = (somatorio / multiplicatorio).to_f
 
 			versiculo.each do |termo, valor|
 				if !@pesoExatas[key]
@@ -456,12 +315,3 @@ class Principal
 		retorno
 	end
 end
-
-# module RSLPStemmer
-#   extend FFI::Library
-
-#    ffi_lib 'c'
-#    DLL = File.expand_path('rslpStemmer.so', File.dirname(__FILE__))
-#   ffi_lib "#{DLL}"
-#   attach_function :rslpLoadStemmer, [ :pointer, :string ], :void
-# end
