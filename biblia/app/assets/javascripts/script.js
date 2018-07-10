62619ad1ff27507fc6bad9c4edc358bfa4cdb24f
Daniel Gomes de Oliveira <daniel22390@hotmail.com>
2018-07-10 00:40:25 -0300

    criado o termo de aceite

--- biblia/app/assets/javascripts/script.js
+++ biblia/app/assets/javascripts/script.js
@@ -19,27 +19,29 @@ $.API = {
 
 $(document).ready(function(){
    $(".cadastra_usuario").submit(function(ev){
-   	ev.preventDefault();
-   	var data = $(this).serialize();
-   	var method = $(this).attr("method");
-   	var action = $(this).attr("action");
-   	$.API.ajax(action, method, data, 'json', function(retorno){
-   		if(retorno.status === "Error"){
-	   		$("#msg_cadastro").text(retorno.message);
-	   		$(".msg_sucesso_cadastro").hide();
-	   		$(".msg_erro_cadastro").show();
-	   	}
-	   	else if(retorno.status = "Success"){
-	   		$("#msg_cadastro_2").text(retorno.message);
-	   		$(".msg_erro_cadastro").hide();
-	   		$(".msg_sucesso_cadastro").show();
-	   	}
-   	}, 
-   	function(xhr,status,error){
-   		$("#msg_cadastro").text(error);
-   		$(".msg_sucesso_cadastro").hide();
-   		$(".msg_erro_cadastro").show();
-   	});
+		if($('.check_aceito').is(':checked')){
+			ev.preventDefault();
+			var data = $(this).serialize();
+			var method = $(this).attr("method");
+			var action = $(this).attr("action");
+			$.API.ajax(action, method, data, 'json', function(retorno){
+				if(retorno.status === "Error"){
+					$("#msg_cadastro").text(retorno.message);
+					$(".msg_sucesso_cadastro").hide();
+					$(".msg_erro_cadastro").show();
+				}
+				else if(retorno.status = "Success"){
+					$("#msg_cadastro_2").text(retorno.message);
+					$(".msg_erro_cadastro").hide();
+					$(".msg_sucesso_cadastro").show();
+				}
+			}, 
+			function(xhr,status,error){
+				$("#msg_cadastro").text(error);
+				$(".msg_sucesso_cadastro").hide();
+				$(".msg_erro_cadastro").show();
+			});
+		}
    });
 
    $(".atualiza_usuario").submit(function(ev){
@@ -79,4 +81,13 @@ $(document).ready(function(){
 	}).then((result) => {
 	});
    });
+
+	$('body').on("change", '.check_aceito', function(ev){
+		if($(this).is(':checked')){
+			$(".btn_cadastro").prop("disabled", false);
+		}
+		else{
+			$(".btn_cadastro").prop("disabled", true);
+		}
+	});
 });
